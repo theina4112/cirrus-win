a0853d5a1eec13ebbce4dc3c0407f3d9c241d6ff
DangDev <54974645+DangDev@users.noreply.github.com>
2021-03-12 20:15:30 +0700

    Update cirrus_pipeline.bat

--- cirrus_pipeline.bat
+++ cirrus_pipeline.bat
@@ -37,8 +37,6 @@ net localgroup administrators ContainerAdministrator /add >nul
 echo To change another VM region, Create New organization (Your current VM location:  %LO% )
 echo Region Available: West Europe, Central US, East Asia, Brazil South, Canada Central, Autralia East, UK South, South India
 echo All done! Connect your VM using RDP. When RDP expired and VM shutdown, Rerun failed jobs to get a new RDP.
-echo IP:
-tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Can't get NGROK tunnel. Maybe your previous VM still running: https://dashboard.ngrok.com/status/tunnels " && exit 
 echo User: Administrator
 echo Pass: Thuonghai001
 curl -O https://gitlab.com/haithuongpro2711/haivps/-/raw/master/Files/DisablePasswordComplexity.ps1 > out.txt 2>&1
