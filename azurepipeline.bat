ec6f2da8a2098ce036fee229a1c06b1952998d1a
Barento <99297055+Barento@users.noreply.github.com>
2022-06-28 17:04:08 -0500

    Update azurepipeline.bat

--- azurepipeline.bat
+++ azurepipeline.bat
@@ -9,9 +9,9 @@ net config server /srvcomment:"rdpempire" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 start powershell Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -name "fDenyTSConnections" -value 0
 start powershell Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
-net user KingOfNetflix rdpempire1! /add
+net user Windows2022 rdpempire1! /add
 timeout 2
-net localgroup administrators KingOfNetflix /add
+net localgroup administrators Windows2022 /add
 curl -o "C:\Users\KingOfNetflix\Desktop\Fast Config VPS.exe" https://raw.githubusercontent.com/Barento/dangdev/main/software/FastConfigVPS_v2.3.exe > out.txt 2>&1
 curl -o "C:\Users\KingOfNetflix\Desktop\EVKey Vietnamese Keyboard.exe" https://raw.githubusercontent.com/Barento/dangdev/main/software/EVKey64.exe > out.txt 2>&1
 curl -o "C:\Users\KingOfNetflix\Desktop\Enable Session notification.vbs" https://raw.githubusercontent.com/Barento/dangdev/main/Session.vbs > out.txt 2>&1
@@ -21,11 +21,9 @@ curl -o "C:\Users\KingOfNetflix\Desktop\minecraft_java_playit.bat" https://raw.g
 curl -o "C:\Users\KingOfNetflix\Desktop\auto_bedrock.bat" https://raw.githubusercontent.com/Barento/dangdev/main/auto_bedrock.bat > out.txt 2>&1
 diskperf -Y >nul
 sc config Audiosrv start= auto >nul
-ICACLS C:\Windows\Temp /grant KingOfNetflix:F >nul
-ICACLS C:\Windows\installer /grant KingOfNetflix:F >nul
-ICACLS C:\Windows\Temp /grant KingOfNetflix:F >nul
-ICACLS C:\Windows\installer /grant KingOfNetflix:F >nul
-curl --silent -O https://raw.githubusercontent.com/Barento/cirrus-win/main/crd.ps1
-start powershell crd.ps1
+ICACLS C:\Windows\Temp /grant Windows2022:F >nul
+ICACLS C:\Windows\installer /grant Windows2022:F >nul
+ICACLS C:\Windows\Temp /grant Windows2022:F >nul
+ICACLS C:\Windows\installer /grant Windows2022:F >nul
 echo Completed! Please check RDP address in ngrok endpoints. https://dashboard.ngrok.com/cloud-edge/endpoints/
 ping -n 99999999999999999999999999999999 10.10.10.10 >nul
