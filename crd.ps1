40d7ad1afa13225fa19ca868f3e2ec460cc9309c
kingofnetflix <72701796+kingofnetflix@users.noreply.github.com>
2022-06-27 19:03:18 -0500

    Create crd.ps1

---
+++ crd.ps1
@@ -0,0 +1,4 @@
+Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
+             & {$P = $env:TEMP + '\chromeremotedesktophost.msi'; Invoke-WebRequest 'https://dl.google.com/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi' -OutFile $P; Start-Process $P -Wait; Remove-Item $P}
+             & {$P = $env:TEMP + '\chrome_installer.exe'; Invoke-WebRequest 'https://dl.google.com/chrome/install/latest/chrome_installer.exe' -OutFile $P; Start-Process -FilePath $P -Args '/install' -Verb RunAs -Wait; Remove-Item $P}
+             & "${Env:PROGRAMFILES(X86)}\Google\Chrome Remote Desktop\CurrentVersion\remoting_start_host.exe" --code="4/0AX4XfWi5x9NhNu8IFp0LUDcsqDkBKhN6eFze2Bs3K8QCItLnEV2l_I0Vp6-GO467htw5Fw" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$Env:COMPUTERNAME -pin 123456
