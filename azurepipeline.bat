659f600dbf2500b87d840ecb1222b462e7eb3de5
theina4112 <106387316+theina4112@users.noreply.github.com>
2022-06-29 11:13:16 +0700

    Update azurepipeline.bat

--- azurepipeline.bat
+++ azurepipeline.bat
@@ -1,7 +1,7 @@
 @echo off
 curl --silent -O https://raw.githubusercontent.com/Barento/cirrus-win/main/ngrok.exe
 timeout 2
-ngrok config add-authtoken 1nUH5O5RlHDFfWb1jlBjQmulJD2_37nVZHH6NKzSq9Fd2KGQH
+ngrok config add-authtoken 217Suv1v07MZGhTfjPvFGx0kh0I_4us95KdwsUJbf24hwUJho
 timeout 2
 start ngrok.exe tcp --region us 3389
 tzutil /s "SE Asia Standard Time" >nul
